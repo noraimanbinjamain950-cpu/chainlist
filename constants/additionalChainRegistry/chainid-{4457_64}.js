21df0591dba12f799ed3d0ed16c37a2f0ebd2b0f
noraimanbinjamain950-cpu <noraimanbinjamain950@gmail.com>
2026-04-25 01:40:30 +0800

    Add Oxin Chain Mainnet configuration
    {
      "name": "Oxin Chain Mainnet",
      "chain": "OXC20",
      "rpc": [
        "https://rpc.oxinchain.io",
      ],
      "faucets": [64],
      "nativeCurrency": {
        "name": "Oxin Coin",
        "symbol": "OXIN",
        "decimals": 9
      },
      "features": [{ "name": "Oxin Chain" }, { "name": "ERC20" }],
      "infoURL": "https://scan.oxinchain.io",
      "shortName": "OXC20",
      "chainId": 4457,
      "networkId": 64,
      "icon": "OXIN",
      "explorers": [{
        "name": "oxinchain",
        "url": "https://scan.oxinchain.io",
        "icon": "oxinchain",
        "standard": "OXC20"
      }]
          }

---
+++ constants/additionalChainRegistry/chainid-{4457_64}.js
@@ -0,0 +1,25 @@
+{
+  "name": "Oxin Chain Mainnet",
+  "chain": "OXC20",
+  "rpc": [
+    "https://rpc.oxinchain.io",
+  ],
+  "faucets": [64],
+  "nativeCurrency": {
+    "name": "Oxin Coin",
+    "symbol": "OXIN",
+    "decimals": 9
+  },
+  "features": [{ "name": "Oxin Chain" }, { "name": "ERC20" }],
+  "infoURL": "https://scan.oxinchain.io",
+  "shortName": "OXC20",
+  "chainId": 4457,
+  "networkId": 64,
+  "icon": "OXIN",
+  "explorers": [{
+    "name": "oxinchain",
+    "url": "https://scan.oxinchain.io",
+    "icon": "oxinchain",
+    "standard": "OXC20"
+  }]
+      }
